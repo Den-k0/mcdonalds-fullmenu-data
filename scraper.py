3baaa59c7a6c646b8363b9910b977c9a3b4916a4
Den_k0 <denys.kotovskiy@gmail.com>
2025-06-15 18:51:58 +0300

    implement scraper for McDonald's menu data

---
+++ scraper.py
@@ -0,0 +1,59 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+HOME_URL = "https://www.mcdonalds.com/ua/uk-ua/eat/fullmenu.html"
+PRODUCT_URL = ("https://www.mcdonalds.com/dnaapp/itemDetails?"
+               "country=UA&language=uk&showLiveData=true&item=")
+
+
+def get_nutrient_value(product_data: dict, nutrient_name: str):
+    nutrient_list = product_data.get("nutrient_facts", {}).get("nutrient", [])
+    for item in nutrient_list:
+        if item.get("name") == nutrient_name:
+            return item.get("value")
+    return None
+
+
+def parse_single_product(product_id: str):
+    response = requests.get(PRODUCT_URL + product_id)
+    response.raise_for_status()
+    product_data = response.json().get("item", {})
+
+    info = {
+        "name": product_data.get("item_name"),
+        "description": product_data.get("description"),
+        "calories": get_nutrient_value(product_data, "Калорійність"),
+        "fats": get_nutrient_value(product_data, "Жири"),
+        "carbs": get_nutrient_value(product_data, "Вуглеводи"),
+        "proteins": get_nutrient_value(product_data, "Білки"),
+        "unsaturated_fats": get_nutrient_value(product_data, "НЖК"),
+        "sugar": get_nutrient_value(product_data, "Цукор"),
+        "salt": get_nutrient_value(product_data, "Сіль"),
+        "portion": get_nutrient_value(product_data, "Вага порції"),
+    }
+
+    return info
+
+
+def get_menu_products():
+    response = requests.get(HOME_URL).content
+    soup = BeautifulSoup(response, "html.parser")
+
+    products_id = [
+        item.get("data-product-id")
+        for item in soup.select(".cmp-category__item")
+    ]
+
+    return [parse_single_product(product_id) for product_id in products_id]
+
+
+def save_to_json(data, filename="menu.json"):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def main():
+    menu = get_menu_products()
+    save_to_json(menu)
